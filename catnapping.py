a5bdab8c3f5e4c5d7f85e1250deeb9727d09fc63
= <simonjordan557@aol.com>
2021-07-20 16:36:41 +0100

    Completed loc 3305

---
+++ catnapping.py
@@ -0,0 +1,14 @@
+spam = '''Dear Alice,
+
+Eve's cat has been arrested for
+catnapping, cat burglary, and
+extortion.
+
+Sincerely,
+Bob'''
+
+print(spam)
+
+eggs = spam.split('\n')
+print(eggs)
+
---
+++ inputValidator.py
@@ -0,0 +1,21 @@
+while True:
+    print('Enter your age:')
+    age = input()
+    if age.isdecimal():
+        break
+    print('Please enter a number for your age.')
+
+while True:
+    print('Enter your name:')
+    name = input()
+    if name.isalpha():
+        break
+    print('Letters only, please.')
+
+while True:
+    print('Select a new password (letters and numbers only):')
+    password = input()
+    if password.isalnum():
+        break
+    print('Passwords must contain only letters and numbers.')
+        
